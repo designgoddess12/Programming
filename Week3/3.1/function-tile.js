be2947d7bd1e0bc3f1e0ae3eb6c453a8df12b260
Heather Huston <130632386+designgoddess12@users.noreply.github.com>
2025-02-06 21:51:38 -0800

    Update function-tile.js

--- Week3/3.1/function-tile.js
+++ Week3/3.1/function-tile.js
@@ -3,27 +3,35 @@ function setup() {
 }
 function createTile() {
     translate(0, 0,);
+     // rectangle
     fill('navy');
+    stroke('black');
+    strokeWeight(5);
     rect(0, 0, 200, 200);
-    stroke('white');
-    strokeWeight(10);
-    line(0, 0, 200, 200);
-    line(30, -20, 200, 200);
-    line(0, -150, 200, 200);
-    line(0, -300, 200, 200);
-    line(0, 40, 200, 200);
-    line(0, 80, 200, 200);
-    line(0, 115, 200, 200);
     noStroke();
-    fill('red');
-    circle(20, 180, 20); 
-    circle(80, 180, 20); 
-    circle(180, 20, 20); 
-    circle(180, 80, 20); 
     fill('white');
-    circle(50, 180, 20); 
-    circle(180, 50, 20);
-}
+     // big white circle
+     noStroke();
+     fill('white');
+     circle(60, 80, 80); 
+     // white lines
+     stroke('white');
+     strokeWeight(5);
+     line(105, 180, 180, 180);
+     line(180, 180, 180, 105);
+     // small red circles
+     noStroke();
+     fill('red');
+     circle(20, 180, 20); 
+     circle(80, 180, 20); 
+     circle(180, 20, 20); 
+     circle(180, 80, 20); 
+     // small middle circles
+     noStroke();
+     fill('white');
+     circle(50, 180, 20); 
+     circle(180, 50, 20);
+ }
 
 function draw () {
     createTile();
